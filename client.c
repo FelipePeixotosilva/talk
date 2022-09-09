5d7a22b99cc49d17195c74f267dee58d71062c1d
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-09-09 19:51:41 -0300

    Update client.c

--- client.c
+++ client.c
@@ -1,1 +1,11 @@
+#include<signal.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
+int main(int argc, char* argv[])
+{
+    int v = atoi(argv[1]);
+    kill(v,SIGUSR2);
+}
