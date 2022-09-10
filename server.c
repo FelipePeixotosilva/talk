25fa3c9908ffb1c1c9e18223515dbaa6d368bcac
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-09-10 19:27:04 -0300

    Update server.c

--- server.c
+++ server.c
@@ -1,20 +1,38 @@
-#include<unistd.h>
-#include<stdio.h>
-#include<signal.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/10 13:52:06 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/09/10 19:18:04 by fpeixoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void soma(int sinal)
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void handler(int sig)
 {
-    if (sinal == SIGUSR1)
-    {
-       // printf("%d", 1 + 1);
-    }
-    if(sinal == SIGUSR2)
-        printf("%d", 2 + 2);   
+    printf("\n");
+    if(sig == SIGUSR1)
+        printf("1");
+    else if(sig == SIGUSR2)
+        printf("0");
+    printf("\n");
 }
 int main()
 {
     int pid = getpid();
-    printf("Thi is : %d\n", pid);
-    signal(SIGUSR2,soma);
-    sleep(5000);
+    printf("Meu pid %d\n", pid);
+    while(1)
+    {
+        signal(SIGUSR1,handler);
+        signal(SIGUSR2,handler);
+        pause();
+    }
+   // sleep(5000);
+    
 }
