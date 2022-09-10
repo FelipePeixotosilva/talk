622330042e6b8e01bddeef46ccb85096d8dd309b
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-09-10 19:27:37 -0300

    Update client.c

--- client.c
+++ client.c
@@ -1,11 +1,69 @@
-#include<signal.h>
-#include<unistd.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/10 13:44:42 by fpeixoto          #+#    #+#             */
+/*   Updated: 2022/09/10 19:23:59 by fpeixoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char* argv[])
+#include <unistd.h>
+#include <signal.h>
+
+int convert(int number, int pid)
 {
-    int v = atoi(argv[1]);
-    kill(v,SIGUSR2);
+    int casa = number;
+    int cont = 1;
+    while(casa > 0)
+    {
+        casa = casa / 2;
+        cont++;
+    }
+    while(cont > 0)
+    {
+        if(number % 2 == 1)
+        {
+            kill(pid,SIGUSR1);
+        }
+        else
+        {
+            kill(pid,SIGUSR2);
+        }
+        usleep(100);
+        number=number / 2;
+        cont--;
+    }
+}
+int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		s;
+	int		j;
+
+	i = 0;
+	s = 1;
+	j = 0;
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || (nptr[i] == 32))
+		i++;
+	if (nptr[i] == '-')
+	{
+		s = -1;
+		i++;
+	}	
+	else if (nptr[i] == '+')
+		i++;
+	while ((nptr[i] != '\0'))
+	{
+		j = j * 10 + (nptr[i] - '0');
+		i++;
+	}	
+	return (j * s);
+}
+int main(int argc, char *argv[])
+{
+    int pid = ft_atoi(argv[1]);
+    convert(65, pid);
 }
