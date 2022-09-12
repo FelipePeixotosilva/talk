c5e11eb1fd5e77d93beebf14d0e65786bc7624d3
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-09-12 20:13:02 -0300

    Update server.c

--- server.c
+++ server.c
@@ -6,14 +6,43 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 13:52:06 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/09/10 19:18:04 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/09/12 18:42:00 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
+static char nbr[7];
+static int val = 0;
+
+int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		s;
+	int		j;
+
+	i = 0;
+	s = 1;
+	j = 0;
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || (nptr[i] == 32))
+		i++;
+	if (nptr[i] == '-')
+	{
+		s = -1;
+		i++;
+	}	
+	else if (nptr[i] == '+')
+		i++;
+	while ((nptr[i] != '\0'))
+	{
+		j = j * 10 + (nptr[i] - '0');
+		i++;
+	}	
+	return (j * s);
+}
 void handler(int sig)
 {
     printf("\n");
@@ -22,6 +51,7 @@ void handler(int sig)
     else if(sig == SIGUSR2)
         printf("0");
     printf("\n");
+    val++;
 }
 int main()
 {
