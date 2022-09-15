88cc87a761c71675858d0b733262c92a9640a5c1
Felipe Peixoto da Silva <felipe.peixoto@msn.com>
2022-09-14 21:18:18 -0300

    Update server.c

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: fpeixoto <fpeixoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 13:52:06 by fpeixoto          #+#    #+#             */
-/*   Updated: 2022/09/13 21:17:46 by fpeixoto         ###   ########.fr       */
+/*   Updated: 2022/09/14 20:27:28 by fpeixoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-int	ft_atoi(const char *nptr)
-{
-	size_t	i;
-	int		s;
-	int		j;
-
-	i = 0;
-	s = 1;
-	j = 0;
-	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || (nptr[i] == 32))
-		i++;
-	if (nptr[i] == '-')
-	{
-		s = -1;
-		i++;
-	}	
-	else if (nptr[i] == '+')
-		i++;
-	while ((nptr[i] != '\0'))
-	{
-		j = j * 10 + (nptr[i] - '0');
-		i++;
-	}	
-	return (j * s);
-}
 
 void handler(int sig)
 {
@@ -56,19 +30,20 @@ void handler(int sig)
 	{
 		//printf("Caracter:");
 		write(1,&c,1);
-		printf("\n");
+		nbr = 0;
+		c = 0;
 	}        
 }
 int main()
 {
     int pid = getpid();
     printf("Meu pid %d\n", pid);
+	
     while(1)
-    {
-        signal(SIGUSR1,handler);
-        signal(SIGUSR2,handler);
+	{
+		signal(SIGUSR1,handler);
+		signal(SIGUSR2,handler);
         pause();
-    }
-   // sleep(5000);
-    
+	}
+   // sleep(5000)
 }
